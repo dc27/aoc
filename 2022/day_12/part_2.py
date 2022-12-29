fb852d030f0efb0127eae68d7f928382167d0786
David Currie <dcurrie27@gmail.com>
2022-12-29 21:38:51 +0000

    adds p2 solution d12

--- 2022/day_12/part_2.py
+++ 2022/day_12/part_2.py
@@ -21,13 +21,12 @@ def letter_lookup(letters, letter):
 letters = string.ascii_lowercase[:]
 
 # grab start and end positions
-end = search_maze(lines, 'E')
-start = search_maze(lines, 'S')
+start = search_maze(lines, 'E')
 
 # pre pathfinding setup
 end_reached = False
 position = start
-lines[start[0]][start[1]] = 'a'
+lines[start[0]][start[1]] = 'z'
 # so letters can be compared after they've been reassigned in the traversing mat
 # kinda like a mirror matrix
 original_mat = list(map(list, lines))
@@ -40,6 +39,8 @@ queue = [position]
 # until reaching the end, set next nodes to 1+ whatever the current pos is,
 # for each of the next noeds set the next next nodes to 1+ whatever the current pos is ...
 
+# for p2 this is essential a reverse from p1 since this time we're looking for new start
+# positions. So start at the end and work backword. Some rules need changed...
 while end_reached == False:
 
     for _ in range(len(queue)):
@@ -50,25 +51,24 @@ while end_reached == False:
         l = position[0], position[1] - 1
         b = position[0] + 1, position[1]
         r = position[0], position[1] + 1
-        
+
         for d in [a, l, b, r]:
             # make sure we can actually go there (within bounds of 2darray)
             if (0 <= d[0] < (len(lines))) & (0 <= d[1] < (len(lines[0]))):
-                # end condition
-                if (lines[d[0]][d[1]] == 'E') & (letter_lookup(letters, original_mat[position[0]][position[1]]) >= 24):
+                # end condition (reverse of part 1 - end at first a reached legally)
+                if (lines[d[0]][d[1]] == 'a') & (letter_lookup(letters, original_mat[position[0]][position[1]]) <= 1):
                     print(f'steps: {count + 1}')
                     end_reached = True
                     break
                 
                 # if unvisited
                 elif lines[d[0]][d[1]] in letters:
-                    # moving condition thing from q. 
-                    if letter_lookup(letters, original_mat[d[0]][d[1]]) <= (letter_lookup(letters, original_mat[position[0]][position[1]]) + 1):
+                    # moving condition thing from q. (reverse of part 1, can go to any letter above current letter or one below current letter)
+                    if (letter_lookup(letters, original_mat[d[0]][d[1]])) >= (letter_lookup(letters, original_mat[position[0]][position[1]]) -1):
                         lines[d[0]][d[1]] = str(count + 1)
                         # add next nodes to queue for next time
                         new = d[0], d[1]
                         queue.insert(0, new) 
     
     count += 1
-    
     
