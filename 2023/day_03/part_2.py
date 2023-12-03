b03fe299fe628b7740b71772855741844702c3e4
David Currie <dcurrie27@gmail.com>
2023-12-03 13:42:27 +0000

    completes part 2 - woo

--- 2023/day_03/part_2.py
+++ 2023/day_03/part_2.py
@@ -1,41 +1,61 @@
+import math
+
+# 0. setup
 file = 'input.txt'
 
 lines = [line.strip() for line in open(file, 'r')]
 
+# it's good to have boundaries
 bounds = [0, 0, len(lines[0]), len(lines)]
 
-number_is_legal = False
+# initial settings 
+number_is_starry = False
 current_number = ''
-numbers = []
 same_number = False
+starry_numbers = dict()
 
+# get loopy, key track of position in doc (line_no) and line (pos)
 for line_no, line in enumerate(lines):
-    print(line)
     
     for pos, char in enumerate(line):
         if char.isdigit():
             same_number = True
             current_number += char
 
-            # lookaround
+            # lookaround (for stars)
             for x_mod in [-1, 0, 1]:
                 for y_mod in [-1, 0, 1]:
                     y_ = line_no + y_mod
                     x_ = pos + x_mod
                     
+                    # boundaries prevent key errors
                     if (bounds[0] <= y_ < bounds[3]) & (bounds[1] <= (x_) < bounds[2]):
-                        if (lines[y_][x_] != '.') and not (lines[y_][x_].isdigit()):
-                            number_is_legal = True
+                        if (lines[y_][x_] == '*'):
+                            # grab the star's position
+                            star_pos = (y_, x_)
+                            number_is_starry = True
                             break
 
         else:
             # add number if legal and reset
-            if same_number & number_is_legal:
-                numbers.append(int(current_number))
+            if same_number & number_is_starry:
+                if star_pos in starry_numbers.keys():
+                    starry_numbers[star_pos].append(int(current_number))
+                else: 
+                    starry_numbers[star_pos] = [int(current_number)]
             same_number = False
             current_number = ''
-            number_is_legal = False
+            number_is_starry = False
             continue
 
 
-print(sum(numbers))
+# 2. calculate gear ratios
+# start cumsum
+sum_of_gear_ratios = 0
+
+for v in starry_numbers.values():
+    if len(v) > 1:
+        gear_ratio = math.prod(v)
+        sum_of_gear_ratios += gear_ratio
+
+print(sum_of_gear_ratios)
