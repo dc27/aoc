b94620a0f926c98b7ccaec715c22be5ff5c93aa7
David Currie <dcurrie27@gmail.com>
2023-12-03 12:04:12 +0000

    completes part 1

---
+++ 2023/day_03/part_2.py
@@ -0,0 +1,41 @@
+file = 'input.txt'
+
+lines = [line.strip() for line in open(file, 'r')]
+
+bounds = [0, 0, len(lines[0]), len(lines)]
+
+number_is_legal = False
+current_number = ''
+numbers = []
+same_number = False
+
+for line_no, line in enumerate(lines):
+    print(line)
+    
+    for pos, char in enumerate(line):
+        if char.isdigit():
+            same_number = True
+            current_number += char
+
+            # lookaround
+            for x_mod in [-1, 0, 1]:
+                for y_mod in [-1, 0, 1]:
+                    y_ = line_no + y_mod
+                    x_ = pos + x_mod
+                    
+                    if (bounds[0] <= y_ < bounds[3]) & (bounds[1] <= (x_) < bounds[2]):
+                        if (lines[y_][x_] != '.') and not (lines[y_][x_].isdigit()):
+                            number_is_legal = True
+                            break
+
+        else:
+            # add number if legal and reset
+            if same_number & number_is_legal:
+                numbers.append(int(current_number))
+            same_number = False
+            current_number = ''
+            number_is_legal = False
+            continue
+
+
+print(sum(numbers))
